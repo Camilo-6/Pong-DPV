a412a9ef8eaf1d7b16b970bc2e14e46b59ac5837
Camilo-6 <jcamilo@ciencias.unam.mx>
2025-02-12 20:48:05 -0600

    comentarios al codigo y pelotita como dvd screensaver

--- Example2_4.cpp
+++ Example2_4.cpp
@@ -4,13 +4,26 @@
 #include <GL/glut.h> //the glut file for windows operations
                      // it also includes gl.h and glu.h for the openGL library calls
 #include <math.h>
+#include <stdio.h>
 
 #define PI 3.1415926535898
 
-double xpos, ypos, ydir, xdir; // x and y position for house to be drawn
-double sx, sy, squash;         // xy scale factors
-double rot, rdir;              // rotation
-int SPEED = 50;                // speed of timer call back in msecs
+// Posicion x y y de la pelota
+double xpos, ypos;
+// Direccion en x y y de la pelota
+double ydir, xdir;
+// Factores de escala en x y y
+double sx, sy, squash;
+// Rotacion
+double rot, rdir;
+// Velocidad de la pelota
+double speed = 3.0;
+// Tamanio x del juego
+int xsize = 640;
+// Tamanio y del juego
+int ysize = 480;
+
+// Matrices de transformacion
 GLfloat T1[16] = {1., 0., 0., 0.,
                   0., 1., 0., 0.,
                   0., 0., 1., 0.,
@@ -25,7 +38,11 @@ GLfloat T[16] = {1., 0., 0., 0.,
                  0., 0., 0., 1.};
 
 #define PI 3.1415926535898
+
+// Numero de puntos para dibujar un circulo
 GLint circle_points = 100;
+
+// Funcion para dibujar un circulo, centrado en (centerx, centery) y radio radius
 void MyCircle2f(GLfloat centerx, GLfloat centery, GLfloat radius)
 {
   GLint i;
@@ -39,21 +56,63 @@ void MyCircle2f(GLfloat centerx, GLfloat centery, GLfloat radius)
   glEnd();
 }
 
-GLfloat RadiusOfBall = 15.;
-// Draw the ball, centered at the origin
+// Radio de la pelota
+GLfloat RadiusOfBall = 10.;
+
+// Funcion para dibujar la pelota, centrada en el origen y con color blanco
 void draw_ball()
 {
-  glColor3f(0.6, 0.3, 0.);
+  glColor3f(1.0, 1.0, 1.0);
   MyCircle2f(0., 0., RadiusOfBall);
 }
 
+// Funcion para mostrar en la ventana
 void Display(void)
 {
-  // swap the buffers
+  // Intercambia los buffers
   glutSwapBuffers();
 
-  // clear all pixels with the specified clear color
+  // Limpia todos los pixeles con el color de limpieza especificado
   glClear(GL_COLOR_BUFFER_BIT);
+
+  // Logica del movimiento de la pelota
+
+  // Movimiento de la pelota en el eje x
+  xpos = xpos + xdir * speed;
+
+  // Movimiento de la pelota en el eje y
+  ypos = ypos + ydir * speed;
+
+  // Experimental, aumentar la velocidad de la pelota
+  speed += 0.001;
+
+  // Si toca la pared izquierda, cambia la direccion de la pelota hacia la derecha
+  if (xpos >= xsize - RadiusOfBall)
+  {
+    xdir = -1;
+    // Imprimomes en consola que choco con la pared izquierda
+    printf("Choco con la pared izquierda\n");
+    printf("la velocidad actual es: %f\n", speed);
+  }
+
+  // Si toca la pared derecha, cambia la direccion de la pelota hacia la izquierda
+  else if (xpos <= RadiusOfBall)
+    xdir = 1;
+
+  // Si toca el techo, cambia la direccion de la pelota hacia abajo
+  if (ypos >= ysize - RadiusOfBall)
+    ydir = -1;
+
+  // Si toca el suelo, cambia la direccion de la pelota hacia arriba
+  else if (ypos <= RadiusOfBall)
+    ydir = 1;
+
+  // Aplicamos la transformacion de la pelota
+  T[12] = xpos;
+  T[13] = ypos;
+  glLoadMatrixf(T);
+
+  /*
   // 160 is max X value in our world
   // Define X position of the ball to be at center of window
   xpos = 80.;
@@ -104,11 +163,13 @@ void Display(void)
     draw_ball();
   */
 
+  /*
   // Translate the bouncing ball to its new position
   T[12] = xpos;
   T[13] = ypos;
   glLoadMatrixf(T);
 
+  // Mueve la pelota a su nueva posicion?
   T1[13] = -RadiusOfBall;
   // Translate ball back to center
   glMultMatrixf(T1);
@@ -119,31 +180,37 @@ void Display(void)
   T1[13] = RadiusOfBall;
   // Translate ball up so bottom is at the origin
   glMultMatrixf(T1);
+  */
 
+  // Dibujar la pelota
   draw_ball();
+  // Actualizar la ventana
   glutPostRedisplay();
 }
 
+// Funcion para cambiar el tamano de la ventana y mantener un sistema de coordenadas constante de 640x480
 void reshape(int w, int h)
 {
-  // on reshape and on startup, keep the viewport to be the entire size of the window
+  // On reshape and on startup, keep the viewport to be the entire size of the window
   glViewport(0, 0, (GLsizei)w, (GLsizei)h);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
 
-  // keep our logical coordinate system constant
-  gluOrtho2D(0.0, 160.0, 0.0, 120.0);
+  // Keep our logical coordinate system constant
+  // Si cambiamos xsize o ysize, hay que cambiar estos valores
+  gluOrtho2D(0.0, 640.0, 0.0, 480.0);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 }
 
+// Funcion para inicializar la ventana y parametros de la pelota
 void init(void)
 {
-  // set the clear color to be white
-  glClearColor(0.0, 0.8, 0.0, 1.0);
-  // initial position set to 0,0
-  xpos = 60;
-  ypos = RadiusOfBall;
+  // Pone el color de limpieza en negro?
+  glClearColor(0.0, 0.0, 0.0, 1.0);
+  // Inicializa la posicion en el centro y parametros de la pelota
+  xpos = 170;
+  ypos = 240;
   xdir = 1;
   ydir = 1;
   sx = 1.;
@@ -152,16 +219,19 @@ void init(void)
   rot = 0;
 }
 
+// Funcion para que todo funcione
 int main(int argc, char *argv[])
 {
-
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-  glutInitWindowSize(320, 240);
-  glutCreateWindow("Bouncing Ball");
+  // Inicializa una ventana de 640x480
+  glutInitWindowSize(xsize, ysize);
+  glutCreateWindow("Pong?");
   init();
+  // Registra las funciones de display y reshape
   glutDisplayFunc(Display);
   glutReshapeFunc(reshape);
+  // Loop para correr
   glutMainLoop();
 
   return 1;
